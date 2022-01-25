fixed bugs in parameters loading and ranges calculation

src/genetic.py
@@ -59,7 +59,7 @@ class GA():
         
         self.params_range = params_range
         self.fixed = fixed
-        self.unfixed_inds =tuple(set(range(len(self.params_range))) - set(fixed.keys()))
+        self.unfixed_inds = tuple(set(range(len(self.params_range))) - set(fixed.keys()))
         self.param_to_index = param_to_index
         self.index_to_param = index_to_param
         
@@ -79,37 +79,39 @@ class GA():
             an array with the model parameters
         """
         all_rt, all_human_rt = [], []
-        res_spatial, res_temporal = {}, {}
+        res = {}
 
         #iterate over stimulus images
         for stim in list(self.data.stim.keys()):
             salient = self.data.stim[stim]['salient']
             
-            parameters = {self.index_to_param[i]: v for i, v in enumerate(parameters)}
-            parameters["threshold"] = parameters["base_threshold"] + salient * parameters["threshold_change"]
-            print("new threshold", parameters["threshold"])
+            #slca requires a single treshold parameter, but we also want to keep both threshold and threshold_change for optimization
+            parameters_lca = {self.index_to_param[i]:v for i,v in enumerate(parameters)}
+            parameters_lca["threshold"] += salient * parameters[self.param_to_index["threshold_change"]]
+            parameters_lca.pop("threshold_change")
+            print("new threshold", parameters_lca["threshold"])
 
             #initialize an SLCA model
-            lca = self.lca_model(stimulus=self.data.stim[stim]['map'], 
-                                 **{k:v for k,v in parameters.items() if k not in ("threshold_change", "base_threshold")})
+            lca = self.lca_model(stimulus=self.data.stim[stim]['map'], params_range=self.params_range, **parameters_lca)
              
-            print(mp.current_process().name + ' LCA started ' + stim + ' ' + ', '.join(
-                [str(v) for v in parameters.values()]))
+            print(mp.current_process().name + ' LCA started ' + stim + ' ' + ', '.join([str(v) for v in parameters]))
             #run the SLCA model and save the results
             coords, rt = lca.run_2dim(self.n_trials, self.trial_length)
             print(mp.current_process().name + ' LCA finished ' + stim + ' ' + ', '.join([str(p) for p in rt]))
 
             trial_coord = np.array(coords, dtype=np.float32)
+            trials_h = list(trial_coord // lca.w)
+            trials_w = list(trial_coord % lca.w)
 
             lca_map = np.zeros(shape=(lca.h, lca.w), dtype=np.float32)
             for i in range(len(rt)):
-                lca_map[lca.h, lca.w] = rt[i]
+                lca_map[int(trials_h[i]), int(trials_w[i])] = rt[i]
                 
-            if len(metric_spatial['sal']) > 0:
+            if len(self.metric_spatial['sal']) > 0:
                 lca_smap = gaussian_filter(lca_map, sigma=1.5)
                 lca_smap = (lca_smap - np.min(lca_smap)) / (np.max(lca_smap) - np.min(lca_smap))
 
-            if len(metric_temporal['all']) > 0:
+            if len(self.metric_temporal['all']) > 0:
                 all_rt.extend(rt)
 
             #iterate over participants
@@ -219,7 +221,7 @@ class GA():
         return self.random_gen(self.gen_size)
 
     def mutate(self, set_params):
-        param_num = np.random.choice(self.inds_unfixed)
+        param_num = np.random.choice(self.unfixed_inds)
         new_params = set_params.copy()
         new_params[param_num] *= np.random.choice([0.95, 1.05])
         if new_params[param_num] < self.params_range[param_num][0]:

src/slca.py
@@ -15,7 +15,7 @@ class SLCA():
     The leaky competing accumulator class in numpy
     """
     def __init__(
-            self, stimulus, dt_t=0.01, leak=0.2, competition=0, self_excit=0,
+            self, stimulus, params_range, dt_t=0.01, leak=0.2, competition=0, self_excit=0,
             w_input=1, w_cross=0, offset=0, noise_sd=0, threshold=5.0):
         """
         Initialize a leaky competing accumulator.
@@ -24,6 +24,8 @@ class SLCA():
         ----------
         stimulus : np.ndarray
             the stimulus image
+        params_range : dict
+            range of parameters
         dt_t : float
             the dt / tao term, representing the time step size
         leak : float
@@ -56,21 +58,53 @@ class SLCA():
         self.offset = offset
         self.noise_sd = noise_sd
         # check params
-        self._check_model_config()
+        self._check_model_config(params_range)
         
-    def _check_model_config(self):
-        if 0 >= self.leak:
-            self.leak = 0.05
-        if 0 >= self.competition:
-            self.competition = 0.05
-        if 0 >= self.self_excit:
-            self.self_excit = 0.1
-        if 0 >= self.dt_t:
-            self.dt_t = 0.01
-        if 0 >= self.noise_sd:
-            self.noise_sd = 0.05
-        if 0 >= self.threshold:
-            self.threshold = 10
+    def _check_model_config(self, params_range):
+        if params_range['dt_t'][0] > self.dt_t: 
+            self.dt_t = params_range['dt_t'][0]
+        elif self.dt_t > params_range['dt_t'][1]:
+            self.dt_t = params_range['dt_t'][1]
+            
+        if params_range['leak'][0] > self.leak: 
+            self.leak = params_range['leak'][0]
+        elif self.leak > params_range['leak'][1]:
+            self.leak = params_range['leak'][1]
+            
+        if params_range['competition'][0] > self.competition: 
+            self.competition = params_range['competition'][0]
+        elif self.competition > params_range['competition'][1]:
+            self.competition = params_range['competition'][1]        
+            
+        if params_range['self_excit'][0] > self.self_excit: 
+            self.self_excit = params_range['self_excit'][0]
+        elif self.self_excit > params_range['self_excit'][1]:
+            self.self_excit = params_range['self_excit'][1]  
+            
+        if params_range['w_input'][0] > self.w_input: 
+            self.w_input = params_range['w_input'][0]
+        elif self.w_input > params_range['w_input'][1]:
+            self.w_input = params_range['w_input'][1] 
+            
+        if params_range['w_cross'][0] > self.w_cross: 
+            self.w_cross = params_range['w_cross'][0]
+        elif self.w_cross > params_range['w_cross'][1]:
+            self.w_cross = params_range['w_cross'][1] 
+            
+        if params_range['noise_sd'][0] > self.noise_sd: 
+            self.noise_sd = params_range['noise_sd'][0]
+        elif self.noise_sd > params_range['noise_sd'][1]:
+            self.noise_sd = params_range['noise_sd'][1] 
+            
+        if params_range['offset'][0] > self.offset: 
+            self.offset = params_range['offset'][0]
+        elif self.offset > params_range['offset'][1]:
+            self.offset = params_range['offset'][1] 
+
+        if params_range['threshold'][0] > self.threshold: 
+            self.threshold = params_range['threshold'][0]
+        elif self.threshold > params_range['threshold'][1]:
+            self.threshold = params_range['threshold'][1]             
             
     def make_weights(self, diag_val, offdiag_val):
         """
@@ -122,10 +156,10 @@ class SLCA_global(SLCA):
     """
 
     def __init__(
-            self, stimulus, dt_t=0.01, leak=0.2, competition=1, self_excit=1,
+            self, stimulus, params_range, dt_t=0.01, leak=0.2, competition=1, self_excit=1,
             w_input=1, w_cross=0, offset=0, noise_sd=0, threshold=1):
         super().__init__(
-            stimulus=stimulus, dt_t=dt_t, leak=leak, competition=competition, self_excit=self_excit,
+            stimulus=stimulus, params_range=params_range, dt_t=dt_t, leak=leak, competition=competition, self_excit=self_excit,
             w_input=w_input, w_cross=w_cross, offset=offset, noise_sd=noise_sd, threshold=threshold)
         # the input / recurrent weights
         self.W_i = self.make_weights(self.w_input, self.w_cross)
@@ -159,10 +193,10 @@ class SLCA_local(SLCA):
     """
 
     def __init__(
-            self, stimulus, dt_t=0.01, leak=0.2, competition=1, self_excit=1,
+            self, stimulus, params_range, dt_t=0.01, leak=0.2, competition=1, self_excit=1,
             w_input=1, w_cross=0, offset=0, noise_sd=0, threshold=1):
         super().__init__(
-            stimulus=stimulus, dt_t=dt_t, leak=leak, competition=competition, self_excit=self_excit,
+            stimulus=stimulus, params_range=params_range, dt_t=dt_t, leak=leak, competition=competition, self_excit=self_excit,
             w_input=w_input, w_cross=w_cross, offset=offset, noise_sd=noise_sd, threshold=threshold)
         # the input / recurrent weights
         self.W_i = self.make_weights(self.w_input, self.w_cross)

train.py
@@ -8,9 +8,9 @@ if __name__ == '__main__':
     parser.add_argument('--config', '-p', type=str, default='config', help='path to the folder with parameters and configurations') 
     parser.add_argument('--data', '-d', type=str, default='data', help='path to the data folder')
     
-    config = parser.parse_args()
-    data_folder = parser.data
-    parameter_folder = parser.config
+    args = parser.parse_args()
+    data_folder = args.data
+    parameter_folder = args.config
     
     basic_loader = ParamsLoader(parameter_folder)
     #parse the parameters of the genetic algorithm
@@ -32,7 +32,8 @@ if __name__ == '__main__':
             data=data, participants=participants, metrics=metrics, n_metrics=n_metrics, lca_model=lca_model,
             trial_length=trial_length, n_trials=n_trials)
     #initial parameter sets
-    params = np.zeros(shape=(gens_num, gen_size, len(params_range)))
+    params = np.zeros(shape=(gens_num, gen_size, len(basic_loader.all_params)))
+    print('init_params', params.shape)
     params[0] = ga.first_gen()
     
     for i, param_set in enumerate(params_init):